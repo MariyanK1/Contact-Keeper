65f7bd706522ad14ce8ebf4a3e4c48192f397a42
MariyanK1 <mariyan.pickup@gmail.com>
2021-03-18 11:06:27 +0200

    added user model and validation

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+app.use(express.json({ extended: false }));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Define Routes
